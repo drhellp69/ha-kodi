1353f0bbd907545e0ac41869c76ee5c7fcff0cd7
BuildTools <unconfigured@null.spigotmc.org>
2021-12-24 16:17:24 +0300

    test

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import yaml
 import requests
+import json
 
 # Чтение конфигурации
 with open('config.yaml') as f:
     config = yaml.safe_load(f)
 
-url = config['url'] + "/api/states/sensor.basement_temperature"
+# Запрос данных API Home Assistant
+def states_ha
+url = config['url'] + "/api/states/group.ha_kodi"
 headers = {
     "Authorization": "Bearer " + config['token'],
 }
 response = requests.request("GET", url, headers=headers)
 
-print(response.text)
+data = json.loads(response.content)   
+
+# Получаем entity_id из группы
+print(data['attributes']['entity_id'])
